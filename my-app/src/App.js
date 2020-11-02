8ee202b5ba4572937c12a32fa36fcbbd373b6d96
TaylorAndrewsUNO <tdandrews@unomaha.edu>
2020-11-02 15:50:46 -0600

    created react app initialization
    Set up axios and initial react environment

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {message: ''};
+    }
+
+    componentDidMount() {
+        const url = 'http://localhost:8080/sayhello?name=Java';
+        axios.get(url).then(response => {
+            console.log('response : ');
+            console.log(response);
+            this.setState({message: response.data});
+        }).catch(error => {
+            console.log('error : ');
+            console.log(error);
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                Message : {this.state.message}
+            </div>
+        );
+    }
+}
+
+export default App;
