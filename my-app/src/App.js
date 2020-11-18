44746c3161238daa309901bb9a145b0a6355e01c
TaylorAndrewsUNO <tdandrews@unomaha.edu>
2020-11-18 17:20:01 -0600

    initialization of pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,10 @@
-import React, {Component} from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {message: ''};
-    }
-
-    componentDidMount() {
-        const url = 'http://localhost:8080/hello?name=Java';
-        axios.get(url).then(response => {
-            console.log('response : ');
-            console.log(response);
-            this.setState({message: response.data});
-        }).catch(error => {
-            console.log('error : ');
-            console.log(error);
-        });
-    }
-
-    render() {
-        return (
-            <div className="App">
-                Message : {this.state.message}
-            </div>
-        );
-    }
+  render() {
+    // ...
+  }
 }
 
 export default App;
