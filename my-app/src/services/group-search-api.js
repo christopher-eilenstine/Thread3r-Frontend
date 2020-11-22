8db20ec6b25e5ad4eef4213eab71a41f540e3855
TaylorAndrewsUNO <tdandrews@unomaha.edu>
2020-11-21 22:16:56 -0600

    init of group api endpoints

--- my-app/src/services/group-search-api.js
+++ my-app/src/services/group-search-api.js
@@ -1,24 +1,38 @@
-//import axios from "axios";
+import axios from "axios";
+import authHeader from "./auth-header";
 
-//const API_URL = "http://localhost:8080/api/groups/";
+const API_URL = "http://localhost:8080/api/groups/";
 
-// const getSubscribed = (userId) => {
-//     return axios
-//       .post(API_URL + "subscribed", {
-//         userId,
-//       })
-//       .then((response) => {
+const getSubscribed = async (userId) => {
+    const response = await axios
+    .get(API_URL + "subscribed", {
+      userId,
+      headers: authHeader()
+    });
+  return response.data;
+  };
 
-//         return response.data;
-//       });
-//   };
+  const searchGroups = async (searchToken) => {
+      const response = await axios
+      .get(API_URL + "search?name=", {
+        searchToken,
+        headers: authHeader(),
+      });
+    return response.data;
+  }
 
-  // const searchGroups = (searchToken) => {
-  //     return axios
-  //       .post(API_URL + "search?name=", {
-  //           searchToken,
-  //       })
-  //       .then((response) => {
-  //           return response.data;
-  //       });
-  // }
+  const getAllGroups = async () => {
+    const response = await axios
+      .get(API_URL, {
+        headers: authHeader()
+      });
+    return response.data;
+  }
+
+  const toExport = {
+    getSubscribed,
+    searchGroups,
+    getAllGroups,
+  }
+  
+  export default toExport;
