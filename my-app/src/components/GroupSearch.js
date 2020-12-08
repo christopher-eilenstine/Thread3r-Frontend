a094ddb5b84c927b67dacdad26cd8f403bc7cef1
TaylorAndrewsUNO <tdandrews@unomaha.edu>
2020-12-08 16:13:53 -0600

    Search implemented

--- my-app/src/components/GroupSearch.js
+++ my-app/src/components/GroupSearch.js
@@ -1,30 +1,54 @@
-import React, { useState, useRef, useEffect } from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
+import React, { useState, useEffect } from "react";
+import { Collapse, Space, Input } from 'antd';
 import GroupSearchAPI from "../services/group-search-api";
 
+const { Panel } = Collapse;
+const { Search } = Input;
+
 const GroupSearch = () => {
-  // const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
   const [allGroups, setAllGroups] = useState([]);
-  // const [filteredGroups, setFilteredGroups] = useState([]);
+  const [filteredGroups, setFilteredGroups] = useState([]);
 
   useEffect(() => {
     GroupSearchAPI.getAllGroups()
     .then((responseJson) => {
       setAllGroups(responseJson);
+      setFilteredGroups(responseJson)
     })
     .catch((error) => {
       console.error(error);
     });
   }, []);
 
-  console.log(allGroups);
-
-  if(allGroups[0] !== undefined){
-    var groupName = allGroups[0].name;
+  if(allGroups !== undefined){
+    var groupNames = [];
+    for(var i = 0; i < filteredGroups.length; i++){
+      groupNames[i] = filteredGroups[i].name;
+      console.log(groupNames);
+    }
   }
-  else{
-    var groupName = "Loading..."
+
+  const searchGroup = (e) => {
+    if(e){
+      const newData = allGroups.filter(
+        function(item){
+          const itemData = item.name
+          ? item.name.toUpperCase()
+          : ''.toUpperCase();
+          const textData = e.toUpperCase();
+          return itemData.indexOf(textData) > -1;
+        }
+      );
+      setFilteredGroups(newData);
+      setSearch(e);
+      console.log(filteredGroups);
+      console.log(search);  
+    }
+    else {
+      setFilteredGroups(allGroups);
+      setSearch(e);
+    }
   }
 
   // if (allGroups){
@@ -36,9 +60,26 @@ const GroupSearch = () => {
 
   return (
     <div>
-      {groupName}
+      <div>
+        <Search placeholder="Search for group" size="large" onSearch={searchGroup} />
+      </div>
+      <div>
+      <ul>
+        {groupNames.map(item => {
+          return <Space direction="vertical">
+            <Collapse collapsible="header" bordered>
+              <Panel header={item}>
+                <p>todo</p>
+              </Panel>
+            </Collapse>
+          </Space>
+        })}
+      </ul>
+      </div>
     </div>
   )
 };
+
+
 
 export default GroupSearch;
