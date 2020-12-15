830bf51652d7cfa477bb2b349afa5a93d5c39b6c
Christopher Eilenstine <ceilenstine@unomaha.edu>
2020-12-14 19:40:07 -0600

    Edit Profile Page

--- my-app/src/components/Profile.js
+++ my-app/src/components/Profile.js
@@ -8,16 +8,9 @@ const Profile = () => {
     <div className="container">
       <header className="jumbotron">
         <h3>
-          <strong>{currentUser.username}</strong> Profile
+          Profile: <strong>{currentUser.username}</strong>
         </h3>
       </header>
-      <p>
-        <strong>Token:</strong> {currentUser.token.substring(0, 20)} ...{" "}
-        {currentUser.token.substr(currentUser.token.length - 20)}
-      </p>
-      <p>
-        <strong>Id:</strong> {currentUser.id}
-      </p>
       <p>
         <strong>Email:</strong> {currentUser.email}
       </p>
