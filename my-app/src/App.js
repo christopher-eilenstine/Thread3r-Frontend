70cb953e49c8423bea3f27ac9a40db686a4325a7
Christopher Eilenstine <ceilenstine@unomaha.edu>
2020-12-14 21:36:13 -0600

    Route Improvements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -99,7 +99,12 @@ const App = () => {
 
       <div className="container mt-3">
         <Switch>
-          <Route exact path={["/", "/home"]} component={Home} />
+          {currentUser ? (
+            <Route exact path={["/", "/home"]} component={Home} />
+          ) : (
+            <Route exact path={["/", "/home"]} component={Login} />
+          )}
+          
           <Route exact path={"/login"} component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/profile" component={Profile} />
